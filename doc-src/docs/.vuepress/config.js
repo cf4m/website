e13af7f5cf9a1d6a91a566d07000c48f1963acb8
Enaium <32991121+Enaium@users.noreply.github.com>
2021-01-22 23:04:34 +0800

    .

---
+++ doc-src/docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    title: 'CF4M',
+    description: 'CF4M Doc',
+    dest: '../docs',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+			{
+                text: 'Languages',
+                items: [
+                    { text: 'English', link: '/' },
+                    { text: 'Chinese', link: '/zh/' }
+                ],
+            },
+            {
+                text: 'Video',
+                items: [
+                    { text: 'Youtube', link: 'https://www.youtube.com/watch?v=zhfln_BeGh8' },
+                    { text: 'BiliBili', link: 'https://www.bilibili.com/video/BV1qK41157gP' }
+                ],
+            },
+            {
+                text: 'GitHub', link: 'https://github.com/cf4m/cf4m',
+            }
+        ]
+    }
+}
