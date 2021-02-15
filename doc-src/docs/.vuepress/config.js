5f9c11f12d8d4b239dd1d4a91076fb7d55c94f30
Enaium <32991121+Enaium@users.noreply.github.com>
2021-02-15 14:45:47 +0800

    .

--- doc-src/docs/.vuepress/config.js
+++ doc-src/docs/.vuepress/config.js
@@ -17,7 +17,7 @@ module.exports = {
     themeConfig: {
         sidebar: 'auto',
         repo: 'cf4m/website',
-        docsDir: 'docs',
+        docsDir: 'doc-src',
         editLinks: true,
 		smoothScroll: true,
         locales: {
